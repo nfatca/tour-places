50a46755a5f5465269fb46275acdc519b69d5e69
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-11-20 03:20:21 +0300

    cc

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,12 @@ import "./Header.scss";
 
 const Header = () => {
   return (
-    <div className="Header">
-      <h1>Popular Tour Placaes</h1>
-    </div>
+    <>
+      <div className="Header">
+        <h1>Popular Tour Placaes</h1>
+      </div>
+      <div></div>
+    </>
   );
 };
 
